9de11872b2322972fa9c9951eb4b8071c85aac1a
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-23 15:01:58 -0700

    Add js fucntions. About to test big things. Might break it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
+function totalValue (number1, number2, number3, number4) {
+  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
+}
+
+
 $(document).ready(function() {
   $(".start-button").click(function() {
     $(".begin-showing").show();
     $(".begin-hidden").hide();
+    event.preventDefault();
+      
   });
 });
 
 
 
 
-  
+let weapon = $("input[name='weapon]:checked").val();
+
+let beverage = $("input[name='beverage']:checked").val();
+
+let land = $("input[name='land']:checked").val();
+
+let total = totalValue (weapon, beverage, land);
+
+$(".result1").hide();
+$(".result2").hide();
+$(".result3").hide();
+$(".result4").hide();
+
+if (total >=14) {
+  $(".result1").show();
+} else if (total >=11) {
+  $(".result2").show(); 
+} else if (total >= 8) {
+  $(".result3").show();
+} else {
+
+}
 
