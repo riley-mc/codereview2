461c02b55787d961b68cdae5e997a9acefe3c906
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-23 13:59:02 -0700

    Fix js that was supposed to hide class.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
-function totalValue (number1, number2, number3, number4)  {
-  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
-
-}
-
 $(document).ready(function() {
-  $("#questions").submit(function(event) {
-    event.preventDefault();
-    
-  }
-
   $(".start-button").click(function() {
     $(".begin-showing").show();
     $(".begin-hidden").hide();
   });
 });
+
+
+
+
+  
 
