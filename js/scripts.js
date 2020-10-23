e925c1ca973dcd8080802dc6ba2d55b280817ff4
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-23 16:31:20 -0700

    One of last commits. Can't get it to work. Won't show results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
+function totalValue (number1, number2, number3, number4) {
+  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
+}
+
+
+
 $(document).ready(function() {
-  $(".start-button").click(function() {
-    $(".begin-showing").show();
-    $(".begin-hidden").hide();
-     event.preventDefault();   
+  // $(".start-button").click(function() {
+  //   $(".begin-showing").show();
+  //   $(".begin-hidden").hide();
+  $("#questions").submit(function(event) {
+    event.preventDefault();   
+    });
   });
 });
-
-
 
 
 let weapon = $("input[name='weapon']:checked").val();
@@ -36,7 +42,4 @@ if (total >=14) {
   $(".result4").show();
 } 
 
-function totalValue (number1, number2, number3, number4) {
-  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
-}
 
