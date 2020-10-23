b05a9088c7a20bdb1f5dec38408e85c4fba60d93
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-23 13:40:31 -0700

    Add different form id. Probably broke it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
+function totalValue (number1, number2, number3, number4)  {
+  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
+
+}
+
 $(document).ready(function() {
+  $("#questions").submit(function(event) {
+    event.preventDefault();
+    
+  }
+
   $(".start-button").click(function() {
     $(".begin-showing").show();
     $(".begin-hidden").hide();
   });
-  });
+});
 
-
-  const brand = $("input:radio[name=brand]:checked").val();
