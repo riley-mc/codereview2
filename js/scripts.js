8fac10ebbbe2c512006f59b35751020c5c668e9e
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-24 13:17:45 -0700

    Add results html and adjusted beginning js. Slow and steady.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
     const drink = $("select#drinkChoice").val();
     const weapon = $("select#weaponChoice").val();
     const treasure = $("select#treasureChoice").val();
-    const land = $("select#landChoice").val();
 
   $("results").show();
   $("span#name").prepent().text(name);
 
-  
+  });
+});
