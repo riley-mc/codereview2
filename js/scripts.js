731b9b8a087694424e7d8bc026ada019497324e1
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-24 11:31:18 -0700

    Add start to html results and first js lines again.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,11 @@
 $(document).ready(function() {
-  $(".start-button").click(function() {
-    $(".begin-showing").show();
-    $(".begin-hidden").hide();
-  $("#questions").submit(function(event) {
-    event.preventDefault();   
-    });
-  });
-});
+  $("form#userData").submit(function(event) {
+    const drink = $("select#drinkChoice").val();
+    const weapon = $("select#weaponChoice").val();
+    const treasure = $("select#treasureChoice").val();
+    const land = $("select#landChoice").val();
 
+  $("results").show();
+  $("span#name").prepent().text(name);
 
-let weapon = $("input[name='weapon']:checked").val();
-
-let beverage = $("input[name='beverage']:checked").val();
-
-let land = $("input[name='land']:checked").val();
-
-let treasure = $("input[name='treasure']:checked").val();
-
-let truth = $("input[name='truth']:checked").val();
-
-let total = totalValue (weapon, beverage, land, treasure, truth);
-
-$(".result1").hide();
-$(".result2").hide();
-$(".result3").hide();
-$(".result4").hide();
-
-if (total >=14) {
-  $(".result1").show();
-} else if (total >=11) {
-  $(".result2").show(); 
-} else if (total >= 8) {
-  $(".result3").show();
-} else if (total >=4) {
-  $(".result4").show();
-} 
-
-
-function totalValue (number1, number2, number3, number4) {
-  return parseInt(number1) + parseInt(number2) + parseInt(number3) + parseInt(number4)
-}
+  
