95675e5076a546062833fe10fac0dcca2e856c81
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-24 13:58:26 -0700

    Finished JS but its still not working.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,35 @@ $(document).ready(function() {
     const drink = $("select#drinkChoice").val();
     const weapon = $("select#weaponChoice").val();
     const treasure = $("select#treasureChoice").val();
+    event.preventDefault();
 
   $("results").show();
   $("span#name").prepent().text(name);
 
+  if(weapon === "theSword") {
+    $("#JavaScriptWarrior").fadeIn();
+    $("#RubyWizard").hide();
+    $("#PythonMonk").hide();
+    $("#CSharpBard").hide();
+  }
+  else if (drink === "theWine" && weapon != "theSword") {
+    $("#CSharpBard").fadeIn();
+    $("#JavaScriptWarrior").hide();
+    $("#RubyWizard").hide();
+    $("#PythonMonk").hide();
+  }
+  else if (drink === "theGoo" && weapon ==="theStaff") {
+    $("#RubyWizard").fadeIn();
+    $("#CSharpBard").hide();
+    $("#JavaScriptWarrior").hide();
+    $("#PythonMonk").hide();
+  }
+  else {
+    $("#PythonMonk").fadeIn();
+    $("#RubyWizard").hide();
+    $("#CSharpBard").hide();
+    $("JavaScriptWarrior").hide();
+  }
+  
+    });
   });
-});
